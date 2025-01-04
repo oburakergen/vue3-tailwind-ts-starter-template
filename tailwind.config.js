3472e1d41e556f1075a196d1889a3097146eb7af
Burak ERGEN <burak.ergen@outlook.com.tr>
2025-01-04 15:10:17 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    /* eslint-disable @typescript-eslint/no-require-imports */
+    require('@tailwindcss/forms'),
+  ],
+}
+
